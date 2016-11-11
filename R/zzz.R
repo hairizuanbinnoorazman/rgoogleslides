8c75b2e871b970f627bd31e56d49b1a25d0567d9
Hairizuan <hairizuanbinnoorazman@gmail.com>
2016-11-12 02:12:17 +0800

    Added the single page slides get API endpoint

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,8 @@
     slides.client.id = "10709400262-28lpv43nui21l1172cup6kh68blvkllq.apps.googleusercontent.com",
     slides.client.secret = "FADkDoE_H0B7j-VytEjTbgaU",
     slides.endpoint.get = "https://slides.googleapis.com/v1/presentations/{presentationId}",
-    slides.endpoint.batchUpdate = "https://slides.googleapis.com/v1/presentations/{presentationId}:batchUpdate"
+    slides.endpoint.batchUpdate = "https://slides.googleapis.com/v1/presentations/{presentationId}:batchUpdate",
+    slides.endpoint.page.get = "https://slides.googleapis.com/v1/presentations/{presentationId}/pages/{pageObjectId}"
   )
   toset <- !(names(op.slides) %in% names(op))
   if (any(toset)) options(op.slides[toset])
