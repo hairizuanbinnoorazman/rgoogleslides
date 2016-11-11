c4fac3f274fe62651229374c206aee22f8c9d7c7
Hairizuan <hairizuanbinnoorazman@gmail.com>
2016-11-12 02:13:21 +0800

    Altered the single page API endpoint to provide the endpoint for single page slides API

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,20 @@
 #' Generate endpoint for the Google Slides API
 #' Temporarily available
 #' @export
-get_endpoint <- function(typeOfEndpoint = "slides.endpoint.get", id = NULL){
+get_endpoint <- function(typeOfEndpoint = "slides.endpoint.get", id = NULL, pageObjectId=NULL){
   # Check that id parameter is a character, if not throw an error
   if(!is.character(id)){
     stop("id is not a character.")
+  }
+  # Check if type of endpoint is slides.endpoint.page.get
+  if(typeOfEndpoint == "slides.endpoint.page.get"){
+    # Check that pageObjectId parameter is a character, if not throw an error
+    if(!is.character(pageObjectId)){
+      stop("pageObjectId is not a character")
+    }
+    url_temp <- gsub("{presentationId}", id, getOption(typeOfEndpoint), fixed=TRUE)
+    url_temp <- gsub("{pageObjectId}", id, url_temp, fixed=TRUE)
+    return(url_temp)
   }
   return(gsub("{presentationId}", id, getOption(typeOfEndpoint), fixed=TRUE))
 }
