449f998d2168e2ee660344bdb6e9a5463ff80d1f
Hairizuan <hairizuanbinnoorazman@gmail.com>
2016-11-12 02:01:25 +0800

    Replace all text function is ready and available for use

--- R/slides.R
+++ R/slides.R
@@ -5,12 +5,15 @@
 get_slides_properties <- function(id = NULL){
   # Get endpoint url
   url <- get_endpoint("slides.endpoint.get", id)
+  # Get auth token
   token <- get_token()
   config <- httr::config(token=token)
+  # Get slide properties
   result <- httr::GET(url, config = config, accept_json())
   if(httr::status_code(result) != 200){
     stop("ID provided does not point towards any slide")
   }
+  # Process and return results
   result_content <- content(result, "text")
   result_list <- fromJSON(result_content)
   return(result_list)
@@ -20,13 +23,29 @@ get_slides_properties <- function(id = NULL){
 #' @importFrom httr config accept_json content
 #' @importFrom jsonlite fromJSON
 #' @export
-replace_all_text <- function(id=NULL, replaceText=NULL, containText=NULL, matchCase=TRUE){
+replace_all_text <- function(id=NULL, replaceText=NULL, text=NULL, matchCase=TRUE){
+  # Get endpoint url
   url <- get_endpoint("slides.endpoint.batchUpdate", id)
+  # Get token
   token <- get_token()
   config <- httr::config(token=token)
-  body_params <- list(replaceText = replaceText, containsText = list(text = text, matchCase = matchCase))
-  result <- httr::POST(url, config = config, accept_json())
+  # Creating the list object
+  replace_all_text_list = list(replaceAllText = list(replaceText = replaceText, containsText = list(text = text, matchCase = matchCase)))
+  body_params <- list(requests = c(replace_all_text_list))
+  # Modify slides
+  result <- httr::POST(url, config = config, accept_json(), body = body_params, encode = "json")
+  # Process results
+  result_content <- content(result, "text")
+  result_list <- fromJSON(result_content)
+  # If endpoint return url status other than 200, return error message
   if(httr::status_code(result) != 200){
-    stop("ID provided does not point towards any slide")
+    stop(result_list$error$message)
+  }
+  # Check the occurencesChanged field
+  occurencesChanged <- result_list$replies$replaceAllText$occurrencesChanged
+  if(is.null(occurencesChanged)){
+    return("No changes applied to slides.")
+  } else {
+    return(paste(occurencesChanged, "occurence(s) changed."))
   }
 }
