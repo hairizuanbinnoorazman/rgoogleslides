16d947f2c093630a054a4a4f481e099af392d89c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2016-11-12 01:04:54 +0800

    Added initial code base for replacing all text in the presentation slide.

--- R/slides.R
+++ R/slides.R
@@ -3,15 +3,30 @@
 #' @importFrom jsonlite fromJSON
 #' @export
 get_slides_properties <- function(id = NULL){
-  # Check that id parameter is a character, if not throw an error
-  if(!is.character(id)){
-    stop("id is not a character.")
-  }
-  url <- paste0(getOption("slides.endpoint"), id)
+  # Get endpoint url
+  url <- get_endpoint("slides.endpoint.get", id)
   token <- get_token()
   config <- httr::config(token=token)
   result <- httr::GET(url, config = config, accept_json())
+  if(httr::status_code(result) != 200){
+    stop("ID provided does not point towards any slide")
+  }
   result_content <- content(result, "text")
   result_list <- fromJSON(result_content)
   return(result_list)
+}
+
+#' Replace all text within the slide
+#' @importFrom httr config accept_json content
+#' @importFrom jsonlite fromJSON
+#' @export
+replace_all_text <- function(id=NULL, replaceText=NULL, containText=NULL, matchCase=TRUE){
+  url <- get_endpoint("slides.endpoint.batchUpdate", id)
+  token <- get_token()
+  config <- httr::config(token=token)
+  body_params <- list(replaceText = replaceText, containsText = list(text = text, matchCase = matchCase))
+  result <- httr::POST(url, config = config, accept_json())
+  if(httr::status_code(result) != 200){
+    stop("ID provided does not point towards any slide")
+  }
 }
