46c6e3eae62fc459eb9108011189377754801991
Hairizuan <hairizuanbinnoorazman@gmail.com>
2016-11-12 23:02:44 +0800

    Completed the create slide function. At the same, initiated the delete object request function

--- R/build.R
+++ R/build.R
@@ -33,13 +33,34 @@ build_create_slide <- function(no_of_slides=1, insertionIndex=NULL,
   iterator <- 1
   while(iterator <= no_of_slides){
     create_slide_list <- list(createSlide=list(slideLayoutReference=list()))
-    if(is.null(layoutId) & is.null(predefinedLayout)){
-      create_slide_list[["createSlide"]][["slideLayoutReference"]][["predefinedLayout"]] <- "BLANK"
-    } else {
+    # Define object id as slide reference
+    if(!is.null(objectId)){
+      create_slide_list[["createSlide"]][["objectId"]] <- objectId[iterator]
+    }
+    # Define insertion index on where the slides to be appended
+    if(!is.null(insertionIndex)){
+      create_slide_list[["createSlide"]][["insertionIndex"]] <- insertionIndex[iterator]
+    }
+    # If layout id is available, use layout id, else use predefinedLayout or else if uses default blank layout
+    if(!is.null(layoutId)){
+      create_slide_list[["createSlide"]][["slideLayoutReference"]][["layoutId"]] <- layoutId[iterator]
+    } else if(!is.null(predefinedLayout)){
       create_slide_list[["createSlide"]][["slideLayoutReference"]][["predefinedLayout"]] <- predefinedLayout[iterator]
+    } else {
+      create_slide_list[["createSlide"]][["slideLayoutReference"]][["predefinedLayout"]] <- "BLANK"
     }
     requests_list[[iterator]] <- create_slide_list
     iterator <- iterator + 1
   }
   return(requests_list)
+}
+
+#' Building delete object request
+#' @export
+build_delete_object <- function(objectId=NULL, requests_list=NULL){
+  # Check to see if there is any requests_list provided. Else, reinitialize it.
+  if(is.null(requests_list)){
+    warning("No/Invalid request_list provided. request_list reinitialized")
+    requests_list <- list()
+  }
 }
