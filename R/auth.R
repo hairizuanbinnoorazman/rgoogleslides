ce051a525ba01680e2b54694d95f16bfcc2c803c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-01-20 00:56:26 +0800

    docs: Update the documentation for the authorize function
    Resolves: #6

--- R/auth.R
+++ R/auth.R
@@ -12,10 +12,12 @@ get_token <- function() {
   .slidesEnv$Token
 }
 
-
 #' Authorize R package to access Google Slides API
-#' @description This is a function to authorize the R package to access
+#' @description This is a function to authorize the R package to access the Googleslides API. If no
+#' client.id and client.secret is provided, the package would provide predefined values.
 #' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
+#' @param client.id OAuth client ID. This is obtained from Google API Credentials
+#' @param client.secret OAuth client secret. This is obtained from Google API Credentials
 #' @export
 authorize <- function(client.id = getOption("slides.client.id"),
                       client.secret = getOption("slides.client.secret")){
