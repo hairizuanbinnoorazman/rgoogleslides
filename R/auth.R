c5815e6bc489a1601050f8f5c44519031bf52cf3
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-05-07 02:24:28 +0800

    chore: Move to follow the R style guides

--- R/auth.R
+++ R/auth.R
@@ -16,12 +16,12 @@ get_token <- function() {
 #' @description This is a function to authorize the R package to access the Googleslides API. If no
 #' client.id and client.secret is provided, the package would provide predefined values.
 #' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
-#' @param client.id OAuth client ID. This is obtained from Google API Credentials
-#' @param client.secret OAuth client secret. This is obtained from Google API Credentials
+#' @param client_id OAuth client ID. This is obtained from Google API Credentials
+#' @param client_secret OAuth client secret. This is obtained from Google API Credentials
 #' @export
-authorize <- function(client.id = getOption("slides.client.id"),
-                      client.secret = getOption("slides.client.secret")){
-  app <- oauth_app(appname = "googleslides", key = client.id, secret = client.secret)
+authorize <- function(client_id = getOption("slides.client.id"),
+                      client_secret = getOption("slides.client.secret")){
+  app <- oauth_app(appname = "googleslides", key = client_id, secret = client_secret)
   endpoint <- oauth_endpoints("google")
   token <- oauth2.0_token(endpoint = endpoint, app = app,
                           scope = c("https://www.googleapis.com/auth/presentations",
