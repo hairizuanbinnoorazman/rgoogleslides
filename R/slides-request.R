2c3afd3d7e078ad83f6fa18bb95839e32b44e96c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-05-07 04:20:28 +0800

    feat: Complete initial migration of the replace all text function into the class. The class works with the current set of post batch methods

--- R/slides-request.R
+++ R/slides-request.R
@@ -1,5 +1,16 @@
 #' @importFrom R6 R6Class
-R6Class("GoogleSlidesRequest",
+temp <- R6Class("GoogleSlidesRequest",
   public = list(
+    requests = list(),
+    add_replace_all_text_request = function(replace_text, text, match_case){
+      new_request <- list("replaceText" = replace_text,
+                         "containsText" = list("text" = text, "matchCase" = match_case))
+      wrap_request <- list("replaceAllText" = new_request)
+      self$requests[[length(self$requests) + 1]] <- wrap_request
+      print("New replace all text request added.")
+    },
+    get_parameters = function(){
+      return(self$requests)
+    }
   )
 )
